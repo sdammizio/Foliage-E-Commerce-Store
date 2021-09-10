f4040ea4cd170e157af6e99a713538d152f8341e
S_Dammizio <77556105+sdammizio@users.noreply.github.com>
2021-09-10 12:06:12 -0400

    updated files

--- Code/JS/newsletter.js
+++ Code/JS/newsletter.js
@@ -16,13 +16,18 @@ newsletterOperations.newsletterPopUp= function(){
     });
 
     $("#newsletterSubmitButton").on("click", function(){
-        $(".popupFlexContainer").empty();
+    const userName = $('#firstName').val();
+
+    $('#firstName').val(userName);
+
+    $(".popupFlexContainer").empty();
+
         const updatedNewsletterHTML = `
        
             <div class="popupFlexContainer">
                 <i id="newsletterModal"class="fas fa-times"></i>
                 <div class="popup-box-content">
-                    <h1 class="headingType3 darkGreen">Thank You!</h1>
+                    <h1 class="headingType3 darkGreen">Thank You ${userName}!</h1>
                     <p class="bodyType2 darkGreen">We look forward to sending you updates and news on a monthly basis. You can subscribe any time by clicking on the link at the bottom of each newsletter email.</p>
                 </div>
             </div>
